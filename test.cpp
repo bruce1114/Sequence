81e83ecaced21103a72d05ec26ec6576397051f7
bruce1114 <40078094+bruce1114@users.noreply.github.com>
2018-06-27 12:04:42 +0800

    final again

--- test.cpp
+++ test.cpp
@@ -8,11 +8,11 @@ int main()
 {
 	string s="dna.txt";
 	Sequence se(s);
-	cout<<se.length()<<endl;
+	cout<<"Length: "<<se.length()<<endl;
 	cout<<"A: "<<se.numberOf('A')<<endl;
 	cout<<"T: "<<se.numberOf('T')<<endl;
 	cout<<"C: "<<se.numberOf('C')<<endl;
 	cout<<"G: "<<se.numberOf('G')<<endl;
-	cout<<se.longestConsecutive().length()<<endl;
+	cout<<se.longestConsecutive()<<endl;
 	cout<<se.longestRepeated()<<endl;
 }
